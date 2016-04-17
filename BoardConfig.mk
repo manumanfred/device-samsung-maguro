b081307ffabd2616be729b7782065e6a6329caad
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2016-04-17 14:25:53 +0300

    Remove unnecessary BOARD_RIL_CLASS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,9 +16,6 @@
 TARGET_BOARD_INFO_FILE := device/samsung/maguro/board-info.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/maguro/bluetooth
 
-# Radio
-BOARD_RIL_CLASS := ../../../device/samsung/maguro/ril
-
 # Use the non-open-source part, if present
 -include vendor/samsung/maguro/BoardConfigVendor.mk
 
